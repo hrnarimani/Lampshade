b8885f869170008788e2e63bbdd9027408bc0076
hrnarimani <hrnarimani65@gmail.com>
2023-08-13 08:42:23 +0330

    first commite - Entity - Entity Base-SEO-ViewModels-IRepo-IAPP

---
+++ ShopManagement.Application.Contracts/ProductCategory/IProductCategoryApplication.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopManagement.Application.Contracts.ProductCategory
+{
+    public  interface IProductCategoryApplication
+    {
+        void Create(CreateProductCategory command);
+        void Edit (EditProductCategory command);
+        Domain.ProductCategoryAgg.ProductCategory GetDetails (long  id);
+        List<ProductCategoryViewModel> Search (ProductCategorySearchModel  searchModel);
+    }
+}
---
+++ ShopManagement.Application.Contracts/ProductCategory/ProductCategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopManagement.Application.Contracts.ProductCategory;
+
+public class ProductCategoryViewModel
+{
+    public string Name { get;  set; }
+    public string Picture { get;  set; }
+    public long Id { get;  set; }
+    public string CreationDate { get; set; }
+    public long ProductsCount { get; set; }
+}
